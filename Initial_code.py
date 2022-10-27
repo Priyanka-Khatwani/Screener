63f9eee2f31c1c76313faef6401b8e012d759fd0
Priyanka-Khatwani <105270721+Priyanka-Khatwani@users.noreply.github.com>
2022-10-27 16:45:40 +0530

    Update Initial_code.py

--- Initial_code.py
+++ Initial_code.py
@@ -46,4 +46,7 @@ def upper_bollinger():
     Upper_df['Name']=name
     Upper_df['Time of Upper Bollinger Breaking']=UpperBollinger
     return Upper_df
-st.button(label='Get Upper Bollinger Break points for Nifty100 stocks',on_click=upper_bollinger())
+  
+def upper_bollinger_call():
+  data=upper_bollinger()
+st.button(label='Get Upper Bollinger Break points for Nifty100 stocks',on_click=st.dataframe(upper_bollinger()))
